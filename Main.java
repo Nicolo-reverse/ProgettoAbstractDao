c10b992548f0f4ab443292117605c22ce9372310
FlowgorithMale <167307252+Nicolo-reverse@users.noreply.github.com>
2025-05-28 18:38:11 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,23 @@
-import java.util.Vector;
-
 public class Main {
     public static void main(String[] args) {
-        SquadraDAO dao = new SquadraDAO();
+        SquadraRepository repo = new SquadraRepository();
 
-        dao.add(new Squadra("Inter", "Inzaghi PIAGNONE", 89));
-        dao.add(new Squadra("Milan", "Conceicao", 75));
-        dao.add(new Squadra("Juventus", "MASSIMILIANO ALLEGRI ALEEE OOO, DRUGHI!", 72));
-        dao.add(new Squadra("Napoli", "Conte", 64));
-        dao.add(new Squadra("Roma", "Ranieri", 62));
+        repo.aggiungiSquadra("Inter", "Inzaghi", 89);
+        repo.aggiungiSquadra("Milan", "Pioli", 75);
+        repo.aggiungiSquadra("Juventus", "Montero", 72);
+        repo.aggiungiSquadra("Napoli", "Calzona", 64);
 
         System.out.println("=== Squadre Serie A ===");
-        Vector<Squadra> squadre = dao.getAll();
-        for (int i = 0; i < squadre.size(); i++) {
-            System.out.println(squadre.get(i));
+        for (int i = 0; i < repo.getTutteLeSquadre().size(); i++) {
+            System.out.println(repo.getTutteLeSquadre().get(i));
         }
 
         System.out.println("\n-- Rimozione della Juventus --");
-        dao.remove(new Squadra("Juventus", "", 0)); // basta solo il nome per equals
+        repo.rimuoviPerNome("Juventus");
 
         System.out.println("\n=== Squadre aggiornate ===");
-        Vector<Squadra> aggiornate = dao.getAll();
-        for (int i = 0; i < aggiornate.size(); i++) {
-            System.out.println(aggiornate.get(i));
+        for (int i = 0; i < repo.getTutteLeSquadre().size(); i++) {
+            System.out.println(repo.getTutteLeSquadre().get(i));
         }
     }
 }
